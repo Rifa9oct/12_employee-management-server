a5517641194eac942bd22c072e3970664459d359
Rifa9oct <kohinuraktherr@gmail.com>
2023-11-29 03:03:16 +0600

    implement payment gateway

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -172,13 +173,29 @@ async function run() {
 
 
 
-
-
     //clear token from cookie
     app.post('/logout', async (req, res) => {
       const user = req.body;
       res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
+
+
+    //payment intent
+    app.post("/create-payment-intent", async(req,res)=>{
+      const {salary} = req.body;
+      const amount = parseInt(salary * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types:['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+
 
 
     // Send a ping to confirm a successful connection
